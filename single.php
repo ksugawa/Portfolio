0a9a0c1aa67bd4a0cb241c9d3fe456d22a5ab21a
k-sugawa <sugawakumiko@gmail.com>
2023-09-07 14:49:33 +0900

    php変更

--- single.php
+++ single.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 <div class="page-head">
-<h2>Work</h2>
-<p class="sub-heading">実績</p>
+    <h2>Work</h2>
+    <p class="sub-heading">実績</p>
 </div>
-<?php while ( have_posts() ) : the_post(); ?>
-<div class="content-inner single-content-inner">
-<h1 class="ttlCol"><?php the_title(); ?></h1>
-<?php the_content(); ?>
-<?php endwhile; ?>
-</div>
-<?php get_footer(); ?>
+<?php while (have_posts()) : the_post(); ?>
+    <div class="content-inner single-content-inner">
+        <h1 class="ttlCol"><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+    </div>
+    <?php get_footer(); ?>
