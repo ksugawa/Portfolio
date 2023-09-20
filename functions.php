9131c541be2cef4cb04fff4858ef946f3e8ef472
k-sugawa <sugawakumiko@gmail.com>
2023-09-20 20:57:50 +0900

    サムネイル画像設定追加

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
-/* ---------- サムネイル画像　設定 ---------- */
+/* ---------- サムネイル画像設定 ---------- */
 function setup_theme() {
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'setup_theme');
+
 
 
 /* ---------- 「投稿」の表記変更 ---------- */
