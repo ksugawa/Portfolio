be88e303334d777cd766fe4ca65552a4f4252969
k-sugawa <sugawakumiko@gmail.com>
2023-09-20 20:55:24 +0900

    thanksページ追加

---
+++ page-thanks.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div class="thanks-container">
+        <div class="thanks-container_inner">
+            <div class="contents">
+                <div class="thanks-heading">
+                    <h1>お問い合わせ</h1>
+                </div>
+                <div class="thanks-heading_line"></div>
+                <div class="thanks-description">
+                    <p>送信が完了しました。</p>
+                    <p>ご連絡させていただきます。</p>
+                </div>
+                <div>
+                    <a href="<?php echo home_url(); ?>">
+                        <div id="return-btn">
+                            <b class="bpo btn">戻る</b>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
