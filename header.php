71cc3ca006190dec0b9550ecfb0d5ed7162d3b1c
k-sugawa <sugawakumiko@gmail.com>
2023-09-20 20:55:12 +0900

    タイトルの取得方法修正

--- header.php
+++ header.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kumikosugawa</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/normalize.css">
+    <title><?php the_title(); ?></title>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css?<?php echo (date('YmdHis')); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,21 +23,20 @@
     <?php endif; ?>
 
     <!--/ ページ専用css -->
-
-
     <?php wp_head(); ?>
 </head>
 
-<body id="<?php echo get_post_field('post_name', get_post()); ?>">
-    <header class="header-container">
-        <div class="sitetitle">
-            <nav class="menu collapse">
-                <ul>
-                    <li><a href="<?php echo esc_url(home_url('/')); ?>">ABOUT</a></li>
-                    <li><a href="<?php echo esc_url(home_url('/')); ?>work/">WORK</a></li>
-                    <li><a href="<?php echo esc_url(home_url('/')); ?>other/">OTHER</a></li>
-                </ul>
-            </nav>
-            <div class="navbtn"><span></span><span></span><span></span></div>
-        </div>
-    </header>
+<body>
+    <div class="wrapper">
+        <header class="header-container">
+            <div class="sitetitle">
+                <nav class="menu collapse">
+                    <ul>
+                        <li><a href="<?php echo esc_url(home_url('/')); ?>">ABOUT</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/')); ?>work/">WORK</a></li>
+                        <li><a href="<?php echo esc_url(home_url('/')); ?>other/">OTHER</a></li>
+                    </ul>
+                </nav>
+                <div class="navbtn"><span></span><span></span><span></span></div>
+            </div>
+        </header>
