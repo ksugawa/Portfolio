0fe1422d984cca7d478613284f6d650a57badcc6
k-sugawa <sugawakumiko@gmail.com>
2023-09-20 20:56:08 +0900

    singleページレイアウト変更

--- single.php
+++ single.php
@@ -1,12 +1,41 @@
 <?php get_header(); ?>
-<div class="page-head">
-    <h2>Work</h2>
-    <p class="sub-heading">実績</p>
-</div>
-<?php while (have_posts()) : the_post(); ?>
-    <div class="content-inner single-content-inner">
-        <h1 class="ttlCol"><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+<div class="single-contents">
+    <?php while (have_posts()) : the_post();
+        $cat = get_the_category();
+        $cat = $cat[0];
+    ?>
+        <div>
+            <div class="tag">
+                <p><?php echo $cat->cat_name; ?></p>
+            </div>
+            <div class="ttlCol">
+                <h1><?php the_title(); ?></h1>
+            </div>
+            <div class="companyname">
+                <p><?php the_field('company-name'); ?></p>
+            </div>
+            <div class="item-info__url">
+                <a href="<?php the_field('url'); ?>" class="flex-r">
+                    <p><?php the_field('url'); ?></p>
+                    <img class="linkout-icon" src="<?php echo get_template_directory_uri(); ?>/assets/public/icon-linkout.svg" alt="">
+                </a>
+            </div>
+        </div>
+        
+        <div>
+            <?php the_post_thumbnail(); ?>
+        </div>
+
+        <article>
+            <?php the_content(); ?>
+
+        </article>
     <?php endwhile; ?>
-    </div>
-    <?php get_footer(); ?>
+    <a href="<?php echo home_url(); ?>/work">
+        <div class="more-btn">
+            <p>VIEW MORE</p>
+        </div>
+    </a>
+
+</div>
+<?php get_footer(); ?>
