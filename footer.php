476aa9d3696b02e982c9d5ef0ff714c2d02ad30d
k-sugawa <sugawakumiko@gmail.com>
2023-09-20 20:54:25 +0900

    footer閉じタグ追加

--- footer.php
+++ footer.php
@@ -3,12 +3,13 @@
     <p class="text_en copyright">© 2022 Kumiko Sugawa -All Rights Reserved</p>
   </div>
 </footer>
+</div>
+</div>
 <script src="https://kit.fontawesome.com/bd26051799.js" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/script.js"></script>
 <?php wp_footer(); ?>
-</div>
 </body>
 
 </html>
