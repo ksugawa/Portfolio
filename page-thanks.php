4c9804f338fb587aa653e29fd9675cdda948b7e6
k-sugawa <sugawakumiko@gmail.com>
2023-09-23 00:39:33 +0900

    フォーム自作、thanksページにメール関連の処理（thanksページエラー）

--- page-thanks.php
+++ page-thanks.php
@@ -1,5 +1,49 @@
 <?php get_header(); ?>
-<div class="thanks-container">
+
+<?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$to =  "sugawakumiko.biz@gmail.com";
+$inquiry = $_POST['message'];
+
+// メール本文
+$message = "お問い合わせがありました。\n";
+$message .= "\n";
+$message .= "以下お問い合わせ内容です。\n";
+$message .= "-----------------------------------------------------\n";
+$message .= "お名前：$name\n";
+$message .= "メールアドレス: $email\n";
+$message .= "お問い合わせ内容:\n";
+$message .= "$inquiry\n";
+$message .= "-----------------------------------------------------\n";
+
+
+// メール本文　ユーザー
+$message_user = "※このメールは自動的に返信されたメールです。\n";
+$message_user .= "\n";
+$message_user .= "この度はお問い合せ頂き誠にありがとうございました。\n";
+$message_user .= "改めてご連絡をさせていただきます。\n";
+$message_user .= "\n";
+$message_user .= "以下お問い合わせ内容です。\n";
+$message_user .= "-----------------------------------------------------\n";
+$message_user .= "お名前：$name\n";
+$message_user .= "メールアドレス: $email\n";
+$message_user .= "お問い合わせ内容:\n";
+$message_user .= "$inquiry\n";
+$message_user .= "-----------------------------------------------------\n";
+
+$headers = array('From' => 'sugawakumiko.biz@gmail.com');
+
+mb_send_mail($to, '件名', $message_user, $headers);
+
+?>
+
+<?php if (mb_send_mail($email, '件名', $message, $headers)) : ?>
+
+    <div class="thanks-container">
         <div class="thanks-container_inner">
             <div class="contents">
                 <div class="thanks-heading">
@@ -21,4 +65,5 @@
             </div>
         </div>
     </div>
+<?php endif; ?>
 <?php get_footer(); ?>
--- template-parts/form.php
+++ template-parts/form.php
@@ -1,60 +1,16 @@
-<?php
-//操作アクションを取得
-$act = isset($_POST["act"]) ? intval($_POST["act"]) : 1;
 
-if ($act == 1) {
-    // セッションデータクリア
-    $contact_data = array();
-    // 初期値セット
-    $err_msg = array();
-    $name = '';
-    $email = '';
-    $message = '';
-} elseif ($act == 2) { // 確認ボタンを押下された場合
-    // POSTデータをセッションに格納
-    $_SESSION["contact_data"] = isset($_POST["contact_data"]) ? $_POST["contact_data"] : array();
-    // セッションデータを配列にセット
-    $contact_data = $_SESSION["contact_data"];
-    // 各項目データをセット
-    $name = isset($contact_data["name"]) ? $contact_data["name"] : "";
-    $email = isset($contact_data["email"]) ? $contact_data["email"] : "";
-    $message = isset($contact_data["message"]) ? $contact_data["message"] : "";
-    // 入力チェック
-    $err_msg = checkInputData($contact_data);
-    if (!$err_msg) {
-        header("Location: https://ksugawa.wp.xdomain.jp/thanks/");
-        exit();
-    }
-} else {
-    // セッションデータ取得
-    $contact_data = isset($_SESSION["contact_data"]) ? $_SESSION["contact_data"] : array();
-    // 各項目データをセット
-    $name = isset($contact_data["name"]) ? $contact_data["name"] : "";
-    $email = isset($contact_data["email"]) ? $contact_data["email"] : "";
-    $message = isset($contact_data["message"]) ? $contact_data["message"] : "";
-}
-?>
 
-<?php if (count($err_msg) > 0) { ?>
-    <div>
-        <ul class="error">
-            <?php foreach ($err_msg as $msg) { ?>
-                <li><?php echo $msg; ?></li>
-            <?php } ?>
-        </ul>
-    </div>
-<?php } ?>
-
-<form action="" method="post">
+<form action="<?php echo esc_url(home_url('/')); ?>thanks/" method="post">
     <label>
-        <input type="text" size="40" placeholder="お名前" name="contact_data[name]" value="<?php echo ($name); ?>">
+        <input type="text" size="40" placeholder="お名前" name="name" value="" required />
     </label>
     <label>
-        <input type="email" size="40" placeholder="メールアドレス" name="contact_data[email]" value="<?php echo ($email); ?>">
+        <input type="email" size="40" placeholder="メールアドレス" name="email" value="" required />
     </label>
     <label>
-        <textarea cols="40" rows="10" placeholder="お問い合わせ内容" name="contact_data[message]"><?php echo ($message); ?></textarea>
+        <textarea cols="40" rows="8" placeholder="お問い合わせ内容" name="message"></textarea>
     </label>
-    <input type="submit" name="btnSubmit" value="送信する">
-    <input type="hidden" name="act" value="2">
+    <button type="submit" class="formbtn type1">
+        <span class="btn-txt">送信する</span>
+    </button>
 </form>
