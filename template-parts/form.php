d80cacb1f150b54262de051e471f98c10226efe4
k-sugawa <sugawakumiko@gmail.com>
2023-09-26 22:41:08 +0900

    autocomplete追加

--- template-parts/form.php
+++ template-parts/form.php
@@ -1,12 +1,12 @@
 <form action="<?php echo esc_url(home_url('/')); ?>thanks/" method="post" id="form">
     <div class="form-control">
         <label for="username"></label>
-        <input type="text" id="username" size="40" placeholder="お名前" name="name" value="" required />
+        <input type="text" id="username" size="40" placeholder="お名前" name="name" value="" autocomplete="name" required />
         <small>Error message</small>
     </div>
     <div class="form-control">
         <label for="email" class="form-control"></label>
-        <input type="email" id="email" size="40" placeholder="メールアドレス" name="email" value="" required />
+        <input type="email" id="email" size="40" placeholder="メールアドレス" name="email" value="" autocomplete="email" required />
         <small>Error message</small>
     </div>
     <div class="form-control">
