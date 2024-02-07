af81e5da4c4ac886acc10c8e985c223f727f7318
k-sugawa <sugawakumiko@gmail.com>
2024-02-07 16:19:56 +0900

    foreachに改修

--- template-parts/my-specialization.php
+++ template-parts/my-specialization.php
@@ -1,50 +1,51 @@
 <div id="my-specialization" class="w-46">
-        <div class="mb-10">
-          <p class="txt_en font-38 font-w-700" id="text-target">My Specialization</p>
-          <p>提供できるもの</p>
+  <div class="mb-10">
+    <p class="txt_en font-38 font-w-700" id="text-target">My Specialization</p>
+    <p>提供できるもの</p>
+  </div>
+  <div>
+    <ul class="flex-r">
+
+      <?php
+      $skills = [
+        [
+          "id" => "skill-card_design",
+          "icon" => "icon-design.svg",
+          "titles" => ["Web Design", "UI/UX Design"],
+          "count" => 13
+        ],
+        [
+          "id" => "skill-card_develop",
+          "icon" => "icon-development.svg",
+          "titles" => ["Development"],
+          "count" => 20
+        ],
+        [
+          "id" => "skill-card_marketing",
+          "icon" => "icon-marketing.svg",
+          "titles" => ["SEO/", "Marketing"],
+          "count" => 4
+        ]
+      ];
+      ?>
+      <?php foreach ($skills as $skill) : ?>
+      <li id="<?= $skill['id'] ?>" class="card">
+        <div class="flex-c center card-front">
+          <div class="card-icon">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/<?= $skill['icon'] ?>" alt="アイコン">
+          </div>
+          <div>
+            <?php foreach ($skill['titles'] as $title) : ?>
+            <p class="txt_en font-w-700"><?= $title ?></p>
+            <?php endforeach; ?>
+          </div>
         </div>
-        <div>
-          <ul class="flex-r">
-            <li id="skill-card_design" class="card">
-              <div class="flex-c center card-front">
-                <div class="card-icon">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-design.svg" alt="">
-                </div>
-                <div>
-                  <p class="txt_en font-w-700">Web Design</p>
-                  <p class="txt_en font-w-700">UI/UX Design</p>
-                </div>
-              </div>
-              <div class="flex-c center card-inner">
-                <p class="txt_en font-w-700 font-48">12</p>
-                <p class="txt_en font-w-700">Project</p>
-              </div>
-            </li>
-            <li id="skill-card_develop" class="card">
-              <div class="flex-c center card-front">
-                <div class="card-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-development.svg" alt=""></div>
-                <div>
-                  <p class="txt_en font-w-700">Development</p>
-                </div>
-              </div>
-              <div class="flex-c center card-inner">
-                <p class="txt_en font-w-700 font-48">10</p>
-                <p class="txt_en font-w-700">Project</p>
-              </div>
-            </li>
-            <li id="skill-card_marketing" class="card">
-              <div class="flex-c center card-front">
-                <div class="card-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-marketing.svg" alt=""></div>
-                <div>
-                  <p class="txt_en font-w-700">SEO/</p>
-                  <p class="txt_en font-w-700">Marketing</p>
-                </div>
-              </div>
-              <div class="flex-c center card-inner">
-                <p class="txt_en font-w-700 font-48">4</p>
-                <p class="txt_en font-w-700">Project</p>
-              </div>
-            </li>
-          </ul>
+        <div class="flex-c center card-inner">
+          <p class="txt_en font-w-700 font-48"><?= $skill['count'] ?></p>
+          <p class="txt_en font-w-700">Project</p>
         </div>
-      </div>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+</div>
--- template-parts/skillset.php
+++ template-parts/skillset.php
@@ -1,94 +1,93 @@
-
 <div id="skill-set" class="w-46">
-<div class="mb-10">
-  <p class="txt_en font-38 font-w-700" id="text-target">Skill Set</p>
-  <p>スキル</p>
-</div>
-<ul class="flex-r border-b a-center t-center mb-4">
-  <li id="skill-set-design">
-    <div>
-      <p class="txt_en font-w-700">Web Design</p>
-      <p class="txt_en font-w-700">UI/UX Design</p>
-    </div>
-  </li>
-  <li id="skill-set-develop">
-    <p class="txt_en font-w-700">Development</p>
-  </li>
-  <li id="skill-set-marketing">
-    <div>
-      <p class="txt_en font-w-700">SEO/</p>
-      <p class="txt_en font-w-700">Marketing</p>
-    </div>
-  </li>
-</ul>
-<div id="design-skill-set" class="skill-set-list">
-  <ul class="flex-r">
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-figma.png" alt="">
+  <div class="mb-10">
+    <p class="txt_en font-38 font-w-700" id="text-target">Skill Set</p>
+    <p>スキル</p>
+  </div>
+  <ul class="flex-r border-b a-center t-center mb-4">
+    <li id="skill-set-design">
+      <div>
+        <p class="txt_en font-w-700">Web Design</p>
+        <p class="txt_en font-w-700">UI/UX Design</p>
       </div>
-      <p>Figma</p>
     </li>
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ps.png" alt="">
-      </div>
-      <p>Photoshop</p>
+    <li id="skill-set-develop">
+      <p class="txt_en font-w-700">Development</p>
     </li>
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ai.png" alt="">
+    <li id="skill-set-marketing">
+      <div>
+        <p class="txt_en font-w-700">SEO/</p>
+        <p class="txt_en font-w-700">Marketing</p>
       </div>
-      <p>Illustrator</p>
-    </li>
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-xd.png" alt="">
-      </div>
-      <p>XD</p>
     </li>
   </ul>
-</div>
-<div id="develop-skill-set">
-  <ul class="grid">
-    <li>
-      <p class="txt_en font-w-700">Html5/Css3</p>
-      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill1.svg" alt=""></div>
-    </li>
-    <li>
-      <p class="txt_en font-w-700">jQuery</p>
-      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill1.svg" alt=""></div>
-    </li>
-    <li>
-      <p class="txt_en font-w-700">JavaScipt</p>
-      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill3.svg" alt=""></div>
-    </li>
-    <li>
-      <p class="txt_en font-w-700">PHP</p>
-      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill2.svg" alt=""></div>
-    </li>
-  </ul>
-</div>
-<div id="seo-skill-set" class="skill-set-list">
-  <ul class="flex-r">
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-gads.png" alt="">
-      </div>
-      <p>Google Ads</p>
-    </li>
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ganalytics.png" alt="">
-      </div>
-      <p>Google <br>Analytics</p>
-    </li>
-    <li class="flex-c center">
-      <div class="flex-c center skill-set-logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-gtagmanager.png" alt="">
-      </div>
-      <p>Google <br>Tag Manager</p>
-    </li>
-  </ul>
-</div>
+  <div id="design-skill-set" class="skill-set-list">
+    <ul class="flex-r">
+
+      <?php
+      $designSkills = [
+        ["name" => "Figma", "icon" => "logo-figma.png"],
+        ["name" => "Photoshop", "icon" => "/logo-ps.png"],
+        ["name" => "Illustrator", "icon" => "logo-ai.png"],
+        ["name" => "XD", "icon" => "logo-xd.png"],
+      ];
+      ?>
+
+      <?php foreach ($designSkills as $design) : ?>
+        <li class="flex-c center">
+          <div class="flex-c center skill-set-logo">
+            <img src="<?= get_template_directory_uri() ?>/assets/images/<?= $design["icon"] ?>" alt="アイコン">
+          </div>
+          <p><?= $design["name"] ?></p>
+        </li>
+      <?php endforeach; ?>
+
+    </ul>
+  </div>
+  <div id="develop-skill-set">
+    <ul class="flex-r">
+
+      <?php
+      $devSkills = [
+        ["name" => "HTML", "icon" => "icon-html.svg"],
+        ["name" => "CSS", "icon" => "icon-css.svg"],
+        ["name" => "JavaScript", "icon" => "icon-js.svg"],
+        ["name" => "TypeScript", "icon" => "icon-ts.svg"],
+        ["name" => "PHP", "icon" => "icon-php.svg"],
+        ["name" => "React", "icon" => "icon-react.svg"],
+        ["name" => "Vue", "icon" => "icon-vue.svg"],
+      ];
+      ?>
+
+      <?php foreach ($devSkills as $dev) : ?>
+        <li class="flex-c center">
+          <div class="flex-c center skill-set-logo">
+            <img src="<?= get_template_directory_uri() ?>/assets/images/<?= $dev["icon"] ?>" alt="アイコン">
+          </div>
+          <p class="txt_en font-w-700"><?= $dev["name"] ?></p>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <div id="seo-skill-set" class="skill-set-list">
+    <ul class="flex-r">
+
+      <?php
+      $seoSkills = [
+        ["name" => "Google Ads", "icon" => "logo-gads.png"],
+        ["name" => "Google\nAnalytics", "icon" => "/logo-ganalytics.png"],
+        ["name" => "Google\nTag Manager", "icon" => "logo-gtagmanager.png"],
+      ];
+      ?>
+
+      <?php foreach ($seoSkills as $seo) : ?>
+      <li class="flex-c center">
+        <div class="flex-c center skill-set-logo">
+          <img src="<?= get_template_directory_uri() ?>/assets/images/<?= $seo["icon"] ?>" alt="">
+        </div>
+        <p><?= nl2br($seo["name"]) ?></p>
+      </li>
+      <?php endforeach; ?>
+
+    </ul>
+  </div>
 </div>
