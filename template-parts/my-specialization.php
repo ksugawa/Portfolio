1c9272c87c25f74b7231094d5ae7421d6aed9923
k-sugawa <sugawakumiko@gmail.com>
2024-02-07 12:00:54 +0900

    font-pageのコンテンツをテンプレート化

---
+++ template-parts/my-specialization.php
@@ -0,0 +1,50 @@
+<div id="my-specialization" class="w-46">
+        <div class="mb-10">
+          <p class="txt_en font-38 font-w-700" id="text-target">My Specialization</p>
+          <p>提供できるもの</p>
+        </div>
+        <div>
+          <ul class="flex-r">
+            <li id="skill-card_design" class="card">
+              <div class="flex-c center card-front">
+                <div class="card-icon">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-design.svg" alt="">
+                </div>
+                <div>
+                  <p class="txt_en font-w-700">Web Design</p>
+                  <p class="txt_en font-w-700">UI/UX Design</p>
+                </div>
+              </div>
+              <div class="flex-c center card-inner">
+                <p class="txt_en font-w-700 font-48">12</p>
+                <p class="txt_en font-w-700">Project</p>
+              </div>
+            </li>
+            <li id="skill-card_develop" class="card">
+              <div class="flex-c center card-front">
+                <div class="card-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-development.svg" alt=""></div>
+                <div>
+                  <p class="txt_en font-w-700">Development</p>
+                </div>
+              </div>
+              <div class="flex-c center card-inner">
+                <p class="txt_en font-w-700 font-48">10</p>
+                <p class="txt_en font-w-700">Project</p>
+              </div>
+            </li>
+            <li id="skill-card_marketing" class="card">
+              <div class="flex-c center card-front">
+                <div class="card-icon"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-marketing.svg" alt=""></div>
+                <div>
+                  <p class="txt_en font-w-700">SEO/</p>
+                  <p class="txt_en font-w-700">Marketing</p>
+                </div>
+              </div>
+              <div class="flex-c center card-inner">
+                <p class="txt_en font-w-700 font-48">4</p>
+                <p class="txt_en font-w-700">Project</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
---
+++ template-parts/skillset.php
@@ -0,0 +1,94 @@
+
+<div id="skill-set" class="w-46">
+<div class="mb-10">
+  <p class="txt_en font-38 font-w-700" id="text-target">Skill Set</p>
+  <p>スキル</p>
+</div>
+<ul class="flex-r border-b a-center t-center mb-4">
+  <li id="skill-set-design">
+    <div>
+      <p class="txt_en font-w-700">Web Design</p>
+      <p class="txt_en font-w-700">UI/UX Design</p>
+    </div>
+  </li>
+  <li id="skill-set-develop">
+    <p class="txt_en font-w-700">Development</p>
+  </li>
+  <li id="skill-set-marketing">
+    <div>
+      <p class="txt_en font-w-700">SEO/</p>
+      <p class="txt_en font-w-700">Marketing</p>
+    </div>
+  </li>
+</ul>
+<div id="design-skill-set" class="skill-set-list">
+  <ul class="flex-r">
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-figma.png" alt="">
+      </div>
+      <p>Figma</p>
+    </li>
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ps.png" alt="">
+      </div>
+      <p>Photoshop</p>
+    </li>
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ai.png" alt="">
+      </div>
+      <p>Illustrator</p>
+    </li>
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-xd.png" alt="">
+      </div>
+      <p>XD</p>
+    </li>
+  </ul>
+</div>
+<div id="develop-skill-set">
+  <ul class="grid">
+    <li>
+      <p class="txt_en font-w-700">Html5/Css3</p>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill1.svg" alt=""></div>
+    </li>
+    <li>
+      <p class="txt_en font-w-700">jQuery</p>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill1.svg" alt=""></div>
+    </li>
+    <li>
+      <p class="txt_en font-w-700">JavaScipt</p>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill3.svg" alt=""></div>
+    </li>
+    <li>
+      <p class="txt_en font-w-700">PHP</p>
+      <div><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-skill2.svg" alt=""></div>
+    </li>
+  </ul>
+</div>
+<div id="seo-skill-set" class="skill-set-list">
+  <ul class="flex-r">
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-gads.png" alt="">
+      </div>
+      <p>Google Ads</p>
+    </li>
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-ganalytics.png" alt="">
+      </div>
+      <p>Google <br>Analytics</p>
+    </li>
+    <li class="flex-c center">
+      <div class="flex-c center skill-set-logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-gtagmanager.png" alt="">
+      </div>
+      <p>Google <br>Tag Manager</p>
+    </li>
+  </ul>
+</div>
+</div>
